8598ce1c87f5a2721ec5bbdb0f5890ce6367c069
Michael Thaler <michael.thaler@dlr.de>
2019-06-24 15:43:52 +0200

    More work on RingBuffer2.

--- src/main/scala/com/mthaler/ringbuffer/RingBuffer2.scala
+++ src/main/scala/com/mthaler/ringbuffer/RingBuffer2.scala
@@ -1,6 +1,26 @@
 package com.mthaler.ringbuffer
 
-class RingBuffer2 private (val capacity: Int, readPos: Int, writePos: Int, count: Int, elems: Array[Any]) {
+import scala.collection.AbstractIterator
 
-  def this(capacity: Int) = this(capacity, readPos = 0, writePos = 0, count = 0, elems = Array.ofDim(capacity))
+class RingBuffer2[A] private (val capacity: Int, readPos: Int, writePos: Int, _count: Int, elems: Array[Any]) {
+
+  def this(capacity: Int) = this(capacity, readPos = 0, writePos = 0, _count = 0, elems = Array.ofDim(capacity))
+
+  def apply(i: Int): A = {
+    if (i >= _count) throw new IndexOutOfBoundsException(i.toString)
+    else elems((readPos + i) % capacity).asInstanceOf[A]
+  }
+
+  def iterator: Iterator[A] = new AbstractIterator[A] {
+
+    private var current = 0
+
+    def hasNext: Boolean = current < _count
+
+    def next(): A = {
+      val res = apply(current)
+      current += 1
+      res
+    }
+  }
 }
